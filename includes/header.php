06ab0c02aaeb0dd171dc0afc92a22c812597e227
Bakodev <musaabdulkabeer19@gmail.com>
2024-02-19 21:18:31 +0100

    feat: register and login auth

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
+<?php
+define('APPURL', 'http://localhost/homeland');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,22 +12,22 @@
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500" />
-    <link rel="stylesheet" href="fonts/icomoon/style.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/fonts/icomoon/style.css" />
 
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" />
-    <link rel="stylesheet" href="css/jquery-ui.css" />
-    <link rel="stylesheet" href="css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
-    <link rel="stylesheet" href="css/mediaelementplayer.css" />
-    <link rel="stylesheet" href="css/animate.css" />
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css" />
-    <link rel="stylesheet" href="css/fl-bigmug-line.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/magnific-popup.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/jquery-ui.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap-datepicker.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/mediaelementplayer.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/animate.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/fonts/flaticon/font/flaticon.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/fl-bigmug-line.css" />
 
-    <link rel="stylesheet" href="css/aos.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/aos.css" />
 
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/style.css" />
 </head>
 
 <body>
